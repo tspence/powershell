87900fb25afff46f413b3a00547c10e4aac61f14
Ted Spence <ted@spence.net>
2025-01-09 10:33:53 -0800

    Continuing to clean up the logic to be more robust

--- ensure-warnings-as-errors.ps1
+++ ensure-warnings-as-errors.ps1
@@ -6,26 +6,33 @@ $value = $args[2]
 # Review all files in this folder and child folders
 if (Test-Path -Path $folderspec) {
     foreach ($path in Get-ChildItem -Recurse "${folderspec}/**/*.csproj") {
+
+        # Load XML using "preserve whitespace" so we can avoid unnecessary github changes
         $xml = [xml]::new()
         $xml.PreserveWhitespace = $true
         $xml.Load($path)
-        $node = $xml | Select-Xml -XPath "//Project/PropertyGroup/${key}"
-        if ($node) {
-            Write-Output "The file '${path}' has project property group ${key} set to '${value}'"
-        } else {
-            Write-Output "File [${path}]: Setting project property group ${key} to '${value}'"
 
-            # Construct the node with indentation before it, and a newline after it, so it looks appealing within the overall csproj file
-            $newNodeText = "  <" + $key + ">" + $value + "</" + $key + ">`n  "
-            Write-Output "1"
-            $newNode = $xml.CreateDocumentFragment()
-            Write-Output "2"
-            $newNode.InnerXml = $newNodeText
-            Write-Output "3"
-            $newChild = $xml.Project.PropertyGroup.AppendChild($newNode)
-            Write-Output "4"
-            $xml.Save($path)
-            Write-Output "5"
+        # We can only proceed if there's a single property group within the csproj file
+        $propertyGroups = $xml.SelectNodes("//Project/PropertyGroup")
+        if ($propertyGroups.Count -ne 1) {
+            Write-Output "There are $($propertyGroups.Count) property groups in the file '${path}'; cannot edit"
+        } else {
+
+            # Look for that property within this property group
+            $node = $propertyGroups[0].SelectSingleNode($key)
+            if ($node) {
+                Write-Output "The file '${path}' has project property group ${key} set to '${value}'"
+            } else {
+                Write-Output "File [${path}]: Setting project property group ${key} to '${value}'"
+
+                # Construct the node with indentation before it, and a newline after it, so it looks appealing within the overall csproj file
+                $newNodeText = "  <" + $key + ">" + $value + "</" + $key + ">`n  "
+                $newNode = $xml.CreateDocumentFragment()
+                $newNode.InnerXml = $newNodeText
+                # We must save the results of this method call to a variable; otherwise it prints the function result to the console which looks ugly
+                $newChild = $xml.Project.PropertyGroup.AppendChild($newNode)
+                $xml.Save($path)
+            }
         }
     }
 } else {
